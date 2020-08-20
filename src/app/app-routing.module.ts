d17776ab11e589a595e13aa9b094ab2ac4403264
anndresfelipe29 <afgomezs@correo.udistrital.edu.co>
2020-08-19 20:25:37 -0500

    Carga inicial del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InicioSesionComponent} from './inicio-sesion/inicio-sesion.component';
+import {PaginaPrincipalComponent} from './pagina-principal/pagina-principal.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'pagina-principal', pathMatch: 'full'},
+  {path: 'inicio-sesion', component: InicioSesionComponent},
+  {path: 'pagina-principal', component: PaginaPrincipalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioSesionComponent,
+    PaginaPrincipalComponent
   ],
   imports: [
     BrowserModule,
